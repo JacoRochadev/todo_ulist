9e6131ac405e5c7e72cd7ecbca673d95e149a4ce
JacoRochaDev <jacofernandes@outlook.com>
2024-01-25 13:27:54 -0300

    layout

--- lib/core/widgets/custom_list_item_widget.dart
+++ lib/core/widgets/custom_list_item_widget.dart
@@ -19,41 +19,53 @@ class CustomListItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     TimestampToDateConvert timestampToDateConvert = TimestampToDateConvert();
-    return ListTile(
-      title: Text(
-        element.description,
-        style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              decoration: TextDecoration.none,
-            ),
-      ),
-      subtitle: Text(
-        timestampToDateConvert(element.createdAt),
-        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              decoration: TextDecoration.none,
-            ),
-      ),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          IconButton(
-            icon: const Icon(
-              Icons.edit,
-              color: Colors.blue,
-            ),
-            onPressed: updateItem,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+            width: .3,
           ),
-          IconButton(
-            icon: const Icon(
-              Icons.delete,
-              color: Colors.red,
-            ),
-            onPressed: deleteItem,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: ListTile(
+          title: Text(
+            element.description,
+            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.none,
+                ),
           ),
-        ],
+          subtitle: Text(
+            timestampToDateConvert(element.createdAt),
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.none,
+                ),
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.edit,
+                  color: Theme.of(context).primaryColor,
+                ),
+                onPressed: updateItem,
+              ),
+              IconButton(
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.red,
+                ),
+                onPressed: deleteItem,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
